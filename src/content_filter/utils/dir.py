21e6b714e0c73354a4a543aa7c28dba4e002a0d9
Ananth-Hari-Narain <ananth.harinarain@gmail.com>
2026-02-06 15:29:35 +0000

    Modified create new file function to only allow strings not Path objects

--- src/content_filter/utils/dir.py
+++ src/content_filter/utils/dir.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-def create_new_file_if_missing(dest_path: Path, procedure, *args, **kwargs):
-    if (dest_path.exists()):
+def create_new_file_if_missing(dest_path: str, procedure, *args, **kwargs):
+    if (Path(dest_path).exists()):
         return dest_path
     else:
         return procedure(*args, **kwargs)
