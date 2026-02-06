0fb7611346efec29969fef45f9dd9e61e2901d19
Ananth-Hari-Narain <ananth.harinarain@gmail.com>
2026-02-06 14:38:36 +0000

    Fixed error where args and kwargs were incorrectly passed into function

--- src/content_filter/utils/dir.py
+++ src/content_filter/utils/dir.py
@@ -4,4 +4,4 @@ def create_new_file_if_missing(dest_path: Path, procedure, *args, **kwargs):
     if (dest_path.exists()):
         return dest_path
     else:
-        return procedure(args, kwargs)
+        return procedure(*args, **kwargs)
