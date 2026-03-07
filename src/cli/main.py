dac3ebe5e33ec5ae4670e48b222e79faf9065546
ah3825 <ah3825@ic.ac.uk>
2026-03-07 16:47:22 +0000

    Moved cli file outside content_filter package

---
+++ src/cli/main.py
@@ -0,0 +1,9 @@
+from content_filter.audio import *
+from content_filter.utils import *
+from content_filter.video import SubtitleFilterer
+import cv2
+import matplotlib.pyplot as plt
+from matplotlib import patches
+
+if __name__ == "__main__":
+    pass
