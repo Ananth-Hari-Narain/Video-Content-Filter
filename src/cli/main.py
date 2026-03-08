46de19c9494328d8f1505c2bc0741694dcb6394f
ah3825 <ah3825@ic.ac.uk>
2026-03-08 17:59:22 +0000

    Added full pipeline code in main

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,9 +1,65 @@
+import cv2
+from content_filter.video import get_bounding_quads
 from content_filter.audio import *
 from content_filter.utils import *
-from content_filter.video import SubtitleFilterer
-import cv2
-import matplotlib.pyplot as plt
-from matplotlib import patches
+import os.path
+import numpy as np
+import json
+import subprocess
 
 if __name__ == "__main__":
-    pass
+    VIDEO_PATH = "/home/ananth/repos/video-content-filter/data/raw/example1.mp4"
+    PROFANITY_PATH = "/home/ananth/repos/video-content-filter/src/content_filter/config/profanity_words.txt"
+    TMP_DIR = "tmp"
+    OUTPUT_PATH = "/home/ananth/repos/video-content-filter/data/processed/"
+    # Filter audio for transcription
+    bad_word_timestamps, censored_audio_path = censor_audio_from_video(VIDEO_PATH, load_profanity(PROFANITY_PATH, TMP_DIR), OUTPUT_PATH, TMP_DIR)
+    transcription_path = os.path.join(TMP_DIR, "transcription.json")
+    if not bad_word_timestamps:
+        print("No profanity detected: video unmodified")
+    else:
+        # Filtering the video
+        test_path = "/home/ananth/repos/video-content-filter/tmp/result.json"
+        print("Identifying profanity on screen...")
+        if not os.path.exists(test_path):
+            quad_map, fps, (w, h), n = get_bounding_quads(VIDEO_PATH, bad_word_timestamps, get_relative_character_widths())
+            with open(os.path.join(TMP_DIR, "results.json"), 'w') as fp:
+                json.dump(quad_map, fp, indent=2)
+        else:
+            with open(test_path, 'r') as file:
+                quad_map = json.load(file)
+            fps = 30
+            w = 1350
+            h = 772
+            n = 6
+        
+        censored_video_path = os.path.join(TMP_DIR, "out.mp4")
+        
+        print("On-screen profanity identified! Drawing censoring boxes in...")
+        cap = cv2.VideoCapture(VIDEO_PATH)
+        out = cv2.VideoWriter(censored_video_path, cv2.VideoWriter.fourcc(*'mp4v'), fps, (w, h))
+        idx = 0
+        ordered_frames = sorted(quad_map.keys())
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            
+            for frame_idx in ordered_frames:
+                if int(frame_idx) <= idx < int(frame_idx) + n:
+                    for quad in quad_map.get(frame_idx, []):
+                        cv2.fillConvexPoly(frame, np.array(quad, dtype=np.int32), (0, 0, 0))
+                    break
+            
+            out.write(frame)
+            idx += 1
+        cap.release()
+        out.release()
+
+        print("Video created! Combining video and audio...")
+        # Combining the video and audio
+        command = f"ffmpeg -i {censored_video_path} -i {censored_audio_path} \
+  -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 {OUTPUT_PATH}".split()
+        subprocess.run(command, check=True)
+        print("Done! Video saved at: ", OUTPUT_PATH)
+        
